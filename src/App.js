d6b4b324a99f94563dec862c5af45681b3fe0a19
jeongil.yang <monday@kakao.com>
2021-12-20 17:36:23 +0900

    Learn state and props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ class Subject extends Component {
   render() {
     return (
       <header>
-        <h1>WEB</h1>
-        world wide web!
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
@@ -14,18 +14,19 @@ class Subject extends Component {
 
 class TOC extends Component {
   render() {
-    return (
+    var lists = [];
+    var data = this.props.data;
+
+    var i = 0;
+    while (i < data.length) {
+      lists.push(<li key={data[i].id}><a href={"content/"+data[i].id}>{data[i].title}</a></li>)
+      i = i + 1;
+    }
+
+    return (      
       <nav>
         <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSSL</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
+          {lists}
         </ul>
       </nav>
     );
@@ -36,20 +37,35 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <h2>HTML</h2>
-        HTML is HypterText Markup Language.
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
     );
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "world wide web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is HyperText ..." },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JS", desc: "JavaScript is for interactive" }
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
-        <TOC></TOC>
-        <Content></Content>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <Subject title="React" sub="For UI!"></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content
+          title="HTML"
+          desc="HTML is HypterText Markup Language."
+        ></Content>
       </div>
     );
   }
