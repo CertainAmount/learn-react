ede00e8f229b112d45af7be7715e7a05985eb387
jeongil.yang <monday@kakao.com>
2021-12-30 14:15:49 +0900

    Add create feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import Control from "./component/Control";
 
 class Subject extends Component {
   render() {
     return (
       <header>
         <h1>
-          <a 
-            href="/" 
-            onClick={function(e) {
+          <a
+            href="/"
+            onClick={function (e) {
               e.preventDefault();
               this.props.onChangePage();
-              }.bind(this)}
-              >{this.props.title}
+            }.bind(this)}
+          >
+            {this.props.title}
           </a>
         </h1>
         {this.props.sub}
@@ -30,23 +32,67 @@ class TOC extends Component {
     while (i < data.length) {
       lists.push(
         <li key={data[i].id}>
-          <a 
-            href={"content/"+data[i].id}
-            onClick={function(id, e) {
+          <a
+            href={"content/" + data[i].id}
+            onClick={function (id, e) {
               e.preventDefault();
               this.props.onChangePage(id);
             }.bind(this, data[i].id)}
-            data-id={data[i].id}>{data[i].title}</a>
-        </li>)
+            data-id={data[i].id}
+          >
+            {data[i].title}
+          </a>
+        </li>
+      );
       i = i + 1;
     }
 
-    return (      
+    return (
       <nav>
-        <ul>
-          {lists}
-        </ul>
+        <ul>{lists}</ul>
       </nav>
+    );
+  }
+}
+
+class ReadContent extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+class CreateContent extends Component {
+  render() {
+    return (
+      <article>
+        <h2>Create</h2>
+        <form
+          action="/create_process"
+          method="post"
+          onSubmit={function (e) {
+            e.preventDefault();
+            this.props.onSubmit(
+              e.target.title.value,
+              e.target.desc.value
+            );
+          }.bind(this)}
+        >
+          <p>
+            <input type="text" name="title" placeholder="title"></input>
+          </p>
+          <p>
+            <textarea name="desc" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit"></input>
+          </p>
+        </form>
+      </article>
     );
   }
 }
@@ -66,34 +112,51 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode:'welcome',
-      selected_content_id:2,
+      max_content_id: 3,
+      mode: "welcome",
+      selected_content_id: 2,
       subject: { title: "WEB", sub: "world wide web!" },
-      welcome:{title:'Welcome', desc:'Hello, React!!'},
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
         { id: 1, title: "HTML", desc: "HTML is HyperText ..." },
         { id: 2, title: "CSS", desc: "CSS is for design" },
-        { id: 3, title: "JS", desc: "JavaScript is for interactive" }
+        { id: 3, title: "JS", desc: "JavaScript is for interactive" },
       ],
     };
   }
-  
+
   render() {
-    var _title, _desc = null;
+    var _title,
+      _desc,
+      _article = null;
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === "read") {
       var i = 0;
-      while(i < this.state.contents.length) {
+      while (i < this.state.contents.length) {
         var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
+        if (data.id === this.state.selected_content_id) {
           _title = this.state.contents[i].title;
           _desc = this.state.contents[i].desc;
           break;
         }
-        i = i +1;
+        i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === "create") {
+      _article = <CreateContent onSubmit={function(_title, _desc) {
+        var max_content_id = this.state.max_content_id + 1;
+        var _contents = this.state.contents.concat(
+          {id:max_content_id, title:_title, desc: _desc}
+        )                
+        this.setState({
+          max_content_id: max_content_id,
+          contents: _contents,
+        });
+         
+      }.bind(this)}></CreateContent>;
     }
 
     return (
@@ -105,18 +168,25 @@ class App extends Component {
             this.setState({
               mode: "welcome",
             });
-            }.bind(this)}
+          }.bind(this)}
         ></Subject>
-        <TOC 
-          onChangePage={function(id) {
+        <TOC
+          onChangePage={function (id) {
             this.setState({
               mode: "read",
-              selected_content_id:Number(id)
+              selected_content_id: Number(id),
             });
           }.bind(this)}
-          data={this.state.contents}>
-        </TOC>
-        <Content title={_title} desc={_desc}></Content>
+          data={this.state.contents}
+        ></TOC>
+        <Control
+          onChangeMode={function (_mode) {
+            this.setState({
+              mode: _mode,
+            });
+          }.bind(this)}
+        ></Control>
+        {_article}
       </div>
     );
   }
